8e1e835f69e189fbcd0bf5e382cbb4c839c99561
Manav Shah <manavshah712@gmail.com>
2026-05-22 16:57:26 +0530

    Add web search tool to agent and update instructions for enhanced functionality

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ from agno.models.openai import OpenAIChat
 from agno.db.sqlite import SqliteDb 
 from textwrap import dedent
 from agno.run import RunContext
+from agno.tools.duckduckgo import DuckDuckGoTools
 from dotenv import load_dotenv
 
 load_dotenv(".env")
@@ -14,6 +15,9 @@ db = SqliteDb(db_file="chat_history_db/chat_history.db")
 
 #session ID creation 
 session_id = "chat_history_for_agent_for_data_science"
+
+#create tool for web search
+web_search = DuckDuckGoTools()
 
 def add_key_point(run_context: RunContext,point:str)->str:
     """
@@ -35,14 +39,16 @@ agent = Agent(
     session_id=session_id,
     user_id="Manav",
     session_state={'key_points':[]},
-    tools=[add_key_point],
+    tools=[add_key_point,web_search],
     instructions=dedent("""
             You are an expert assistant. Your task is to:
             1. Create summary on the topic and stick to the word count if provided
             2. You have access to a tool called as 'add_key_point' which adds a key_point
                 from the summary to the session state.
-            3. Add key point only when asked for.
-            4. The Summary created should include both advantages and disadvantages.
+            3. You have the capability to access the web using web search tool. 
+                try to generate summary with the latest information.             
+            4. Add key point only when asked for.
+            5. The Summary created should include both advantages and disadvantages.
                         """),
     add_history_to_context=True,
     add_session_state_to_context=True,
@@ -51,7 +57,7 @@ agent = Agent(
     stream=True
 )
 
-agent.print_response("I need short answers on topic of Data Science in today's world, it's all most 100 words.")
+agent.print_response("I need short answers on topic of AI Agents and its future, it's all most 300 words.")
 
 agent.print_response("add key points from the summary generated above. The number of points depends on the summary.")
 
